077f58182727e82bc0f0b02396ae1ddac27283aa
vizvarych <47158197+vizvarych@users.noreply.github.com>
2019-04-09 00:46:49 +0300

    Update FootballTeam.java
    add sorting

--- src/main/java/team/FootballTeam.java
+++ src/main/java/team/FootballTeam.java
@@ -7,13 +7,34 @@ import com.jayway.jsonpath.JsonPath;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Map;
+
 
 
 public class FootballTeam {
     public static void main(String[] args) throws InvalidJsonException, IOException {
         FileInputStream fis = new FileInputStream("src/main/resources/football_club.json");
         Object document = Configuration.defaultConfiguration().jsonProvider().parse(fis, "UTF-8");
-        ArrayList<String> clubsFoundDate = JsonPath.read(document, "$.football_club[*].founded");
-        System.out.println(clubsFoundDate);
+         ArrayList<Map<String,String>> clubsFoundDate = JsonPath.read(document, "$.football_club[*]");
+
+        Collections.sort(clubsFoundDate, new Comparator<Map<String, String>>() {
+            public int compare(Map<String, String> o1, Map<String, String> o2) {
+                int compare = o1.get("founded").compareTo(o2.get("founded"));
+                if(compare == 0){
+                    compare = o1.get("city").compareTo(o2.get("city"));
+                    if(compare == 0){
+                        compare = o1.get("coach_name").compareTo(o2.get("coach_name"));
+                    }
+                }
+                return compare;
+            }
+        });
+
+        for (Map<String,String> e: clubsFoundDate
+             ) {
+            System.out.println(e);
+        }
     }
 }
